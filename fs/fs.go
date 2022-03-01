ae7afbfe0ddd72ffef5082331c04aa6545e2311e
aaroluo <aaro.luo@gmail.com>
2022-03-01 15:52:01 +0200

    skip hidden files

--- fs/fs.go
+++ fs/fs.go
@@ -5,6 +5,7 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"strings"
 )
 
 type DirEntry struct {
@@ -14,6 +15,42 @@ type DirEntry struct {
 	DeletionInProgress bool
 }
 
+func Traverse(dir string, searchDirs, ignoreDirs []string, searchAll bool, ch chan DirEntry) {
+	defer close(ch)
+
+	// Scan the file tree starting from current working directory
+	// Don't return err because it stops the walk
+	// Instead just log it and skip the directory
+	filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
+		if err != nil {
+			log.Println(err)
+			return filepath.SkipDir
+		}
+		if d.IsDir() {
+			if contains(searchDirs, d.Name()) {
+				size, err := getDirectorySize(path)
+				if err != nil {
+					log.Println(err)
+					return filepath.SkipDir
+				}
+				ch <- DirEntry{Path: path, Size: size}
+				return filepath.SkipDir
+			}
+			if contains(ignoreDirs, d.Name()) ||
+				(strings.HasPrefix(d.Name(), ".") && !searchAll) {
+				log.Println("Skipping ", path)
+				return filepath.SkipDir
+			}
+		}
+		return err
+	})
+}
+
+func Delete(path string) error {
+	return os.RemoveAll(path)
+}
+
+// Recursively search every file and return the total sum
 func getDirectorySize(path string) (int64, error) {
 	var size int64
 	err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
@@ -35,46 +72,7 @@ func getDirectorySize(path string) (int64, error) {
 	return size, nil
 }
 
-func Traverse(searchDirs, ignoreDirs []string, ch chan DirEntry) {
-	// Get current working directory
-	dir, err := os.Getwd()
-	if err != nil {
-		log.Fatal(err)
-	}
-	// dir := "C:\\Users\\aarol\\"
-	defer close(ch)
-
-	// Scan the file tree starting from current working directory
-	// Don't return err because it stops the walk
-	// Instead just skip the directory
-	filepath.WalkDir(dir, func(path string, d fs.DirEntry, err error) error {
-		if err != nil {
-			log.Println(err)
-			return filepath.SkipDir
-		}
-		if d.IsDir() {
-			if contains(d.Name(), searchDirs) {
-				size, err := getDirectorySize(path)
-				if err != nil {
-					log.Println(err)
-					return filepath.SkipDir
-				}
-				ch <- DirEntry{Path: path, Size: size}
-				return filepath.SkipDir
-			}
-			if contains(d.Name(), ignoreDirs) {
-				return filepath.SkipDir
-			}
-		}
-		return err
-	})
-}
-
-func Delete(path string) error {
-	return os.RemoveAll(path)
-}
-
-func contains(e string, s []string) bool {
+func contains(s []string, e string) bool {
 	for _, a := range s {
 		if a == e {
 			return true

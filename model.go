a27fd5f2de515ad96c4e3b72f30a25b58a72ff72
aaroluo <aaro.luo@gmail.com>
2022-03-01 15:52:53 +0200

    add more flags

--- model.go
+++ model.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"log"
+	"os"
 	"strings"
 
 	"github.com/aarol/goclean/fs"
@@ -12,32 +14,57 @@ import (
 )
 
 type model struct {
-	keys           keyMap
-	viewportReady  bool
+	// Config options
+	searchPath  string
+	searchDirs  []string
+	excludeDirs []string
+	searchAll   bool
+
+	viewportReady bool
+	height        int
+	cursor        int
+
 	searchFinished bool
-	searchDirs     []string
-	excludeDirs    []string
-	directories    []fs.DirEntry
-	sub            chan fs.DirEntry
-	viewport       viewport.Model
-	spinner        spinner.Model
-	help           help.Model
 	bytesSaved     int64
-	cursor         int
-	height         int
+
+	directories []fs.DirEntry
+	sub         chan fs.DirEntry // Will receive directories from fs
+
+	keys     keyMap
+	viewport viewport.Model
+	spinner  spinner.Model
+	help     help.Model
 }
 
 func initialModel(c *cli.Context) model {
+
+	// Get search path
+	var path string
+	var err error
+	if c.Bool("home") {
+		path, err = os.UserHomeDir()
+	} else {
+		path, err = os.Getwd()
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	s := spinner.NewModel()
 	s.Spinner = spinner.Dot
 	s.Style = lipgloss.NewStyle().Foreground(lipgloss.Color("205"))
+
 	return model{
-		keys:        keys,
-		directories: []fs.DirEntry{},
-		sub:         make(chan fs.DirEntry),
+		searchPath:  path,
+		searchAll:   c.Bool("all"),
 		searchDirs:  c.Args(),
 		excludeDirs: strings.Split(c.String("exclude"), " "),
-		help:        help.New(),
-		spinner:     s,
+
+		directories: []fs.DirEntry{},
+		sub:         make(chan fs.DirEntry),
+
+		keys:    keys,
+		help:    help.New(),
+		spinner: s,
 	}
 }
